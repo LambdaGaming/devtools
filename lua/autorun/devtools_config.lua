f985ac6aa0f58b52dffb9ce29fe34ec24ea8e5d5
LambdaGaming <starwars18m@gmail.com>
2019-05-28 19:33:40 -0400

    Added tool that gets the vector and angle of the player using it.

--- lua/autorun/devtools_config.lua
+++ lua/autorun/devtools_config.lua
@@ -1,3 +1,3 @@
 
-CreateConVar( "DevTools_ShouldClipboard", 1, FCVAR_NONE, "Whether or not the text the tool prints out should also be copied to your clipboard. (Default 1)" )
-CreateConVar( "DevTools_ShouldFancyText", 1, FCVAR_NONE, "Whether or not the text should print with Vector() or just the numbers alone. (Default 1)" )
+CreateConVar( "DevTools_ShouldClipboard", 1, FCVAR_NONE, "Whether or not the text the tool prints out should also be copied to your clipboard." )
+CreateConVar( "DevTools_ShouldFancyText", 1, FCVAR_NONE, "Whether or not the text should print with Vector() or just the numbers alone." )
---
+++ lua/weapons/gmod_tool/stools/getposangself.lua
@@ -0,0 +1,50 @@
+
+TOOL.Name = "Get Pos and Angle"
+TOOL.Category = "Dev Tools"
+
+if CLIENT then
+	language.Add( "tool.getposang.name", "Get Self Pos and Angle" )
+	language.Add( "tool.getposang.desc", "Prints position and angle of yourself." )
+	language.Add( "tool.getposang.0", "Left-click: Get vector of yourself. Right-click: Get angle of yourself." )
+end
+
+local canfancytext = GetConVar( "DevTools_ShouldFancyText" ):GetBool()
+local canclipboard = GetConVar( "DevTools_ShouldClipboard" ):GetBool()
+
+function TOOL:LeftClick()
+	if IsFirstTimePredicted() and CLIENT then
+		local pos = self.Owner:GetPos()
+		local formattedpos = "Vector( "..pos.x..", "..pos.y..", "..pos.z.." )"
+		if canfancytext then
+			self.Owner:ChatPrint( formattedpos )
+		else
+			self.Owner:ChatPrint( pos )
+		end
+		if canclipboard then
+			if canfancytext then
+				SetClipboardText( formattedpos )
+			else
+				SetClipBoardText( pos )
+			end
+		end
+	end
+end
+
+function TOOL:RightClick()
+	if IsFirstTimePredicted() and CLIENT then
+		local ang = self.Owner:GetAngles()
+		local formattedpos = "Angle( "..ang.x..", "..ang.y..", "..ang.z.." )"
+		if canfancytext then
+			self.Owner:ChatPrint( formattedpos )
+		else
+			self.Owner:ChatPrint( pos )
+		end
+		if canclipboard then
+			if canfancytext then
+				SetClipboardText( formattedpos )
+			else
+				SetClipBoardText( pos )
+			end
+		end
+	end
+end
