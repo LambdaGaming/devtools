fb269c78e04b84dcc56a736642ad19c4df9d3f2d
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-05-31 01:38:38 -0400

    Keep track of version number

--- lua/autorun/server/devtools_server.lua
+++ lua/autorun/server/devtools_server.lua
@@ -4,3 +4,6 @@ function DevTools_TryClipboard( ply, text )
 	net.WriteString( text )
 	net.Send( ply )
 end
+
+local version = "1.9.1"
+MsgC( Color( 0, 255, 255 ), "Development Tool Collection v"..version.." by OPGman successfully loaded.\n" )
